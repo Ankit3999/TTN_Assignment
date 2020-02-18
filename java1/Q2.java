b16979d318bd278f6020479bbd52bf66549a3bc5
Ankit3999 <ankit.sagar@tothenew.com>
2020-02-18 11:51:44 +0530

    java1 assignment

--- java1/Q2.java
+++ java1/Q2.java
@@ -11,13 +11,14 @@ public class Q2 {
         for(int i=0;i<arr.length;i++){
             count=1;
             for(int j=i+1;j<arr.length;j++){
-                if(arr[i].equals(arr[j])){
+                if(arr[i]!= null && arr[i].equals(arr[j])){
                     count++;
-                    arr[j]="0";
+                    arr[j]=null;
                 }
             }
-            if(count > 1 && arr[i] != "0")
-                System.out.println(arr[i]);
+            if(count > 1 && arr[i] != null) {
+                System.out.println(arr[i]+" "+count);
+            }
         }
     }
 }
